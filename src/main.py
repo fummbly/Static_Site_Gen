bafec62fd8e03e09da26c31112fb98b20ec78931
fummbly <tstone449@gmail.com>
2024-06-06 08:57:44 -0400

    Added comments to main and textnode

--- src/main.py
+++ src/main.py
@@ -2,21 +2,24 @@ from textnode import TextNode
 
 
 def main():
+    # Initializing TextNodes 
     firstNode = TextNode("Test text body", "Bold")
-
     secondNode = TextNode("Another text node", "Italic", "http://google.com")
-
     thirdNode = TextNode("Test text body", "Bold")
-
+    
+    # Testing printing of TextNodes
     print(firstNode)
     print(secondNode)
     print(thirdNode)
 
+    # Testing equal function of TextNodes
+    # Should return false
     if firstNode == secondNode:
         print("First node equals second node")
     else:
         print("First node doesn't equal second node")
 
+    # Should return true
     if firstNode == thirdNode:
         print("First node equals third node")
     else:
--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,12 @@
 
 class TextNode:
+    # TextNode constructor with string text, string text_type, string url
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
+    # Overide equals function where true if all properties of the 2 nodes are equal
     def __eq__(self, OtherTextNode):
         return (
             self.text == OtherTextNode.text
@@ -12,5 +14,6 @@ class TextNode:
             and self.url == OtherTextNode.url
         )
 
+    # String representation overide
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
