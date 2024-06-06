6e13d734630a2043d31be3fb8cfbf1b15878ab0e
fummbly <tstone449@gmail.com>
2024-06-06 11:24:25 -0400

    Created LeafNode and added tests for it

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -17,3 +17,34 @@ class TestHTMLNode(unittest.TestCase):
 
         self.assertEqual("HTMLNode(Tag: p, Value: This is a paragraph, Children: None, Props: None)", repr(node))
         self.assertEqual("HTMLNode(Tag: a, Value: This is a link, Children: None, Props: {'href': 'https://www.google.com'})", repr(node2))
+
+    # Testing LeafNode for proper html formating
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        
+    # Testing no tag html formating
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    # Testing tag with props
+    def test_to_html_props(self):
+        node = LeafNode("a", "Google", {"href": "https://www.google.com"})
+
+        self.assertEqual('<a href="https://www.google.com">Google</a>', node.to_html())
+
+    # Testing exception for no value
+    def test_to_html_no_value(self):
+        node = LeafNode(None, None)
+
+        self.assertRaises(ValueError, node.to_html)
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
