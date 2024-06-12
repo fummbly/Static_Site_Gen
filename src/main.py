e3bb927100fce24d16eeb76175a431d7181ef341
fummbly <tstone449@gmail.com>
2024-06-11 21:31:28 -0400

    Added block_markdown

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from htmlnode import ParentNode, LeafNode
-from inline_markdown import extract_markdown_images, split_nodes_image, split_nodes_links, text_to_text_nodes
-from textnode import TextNode
+from block_markdown import markdown_to_blocks
 
 def main():
-    text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
-    print(text_to_text_nodes(text))
+    text = """This is **bolded** paragraph
+
+This is another paragraph with *italic* text and `code` here    
+This is the same paragraph on a new line
+
+* This is a list
+* with items"""
+    blocks = markdown_to_blocks(text)
+    for block in blocks:
+        print(block)
+        print("\n")
+
 
 
 main()
