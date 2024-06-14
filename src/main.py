e24a4582404a6010a10e550e63bdd0c276e0a8f6
fummbly <tstone449@gmail.com>
2024-06-13 21:12:36 -0400

    Created copy_static function

--- src/main.py
+++ src/main.py
@@ -1,30 +1,32 @@
-from block_markdown import markdown_to_html_node
-from htmlnode import ParentNode
+from os import listdir, path, mkdir
+from shutil import rmtree, copy
+
+check_directory(dest_directory):
+
+
+def copy_static(current_directory, dest_directory):
+    
+    if not path.isdir(current_directory) or not path.exists(current_directory):
+        raise Exception(f"{current_directory} doesn't exist or isn't a directory ")
+
+        
+
+    if not path.exists(public_path):
+        print("Making dir")
+        mkdir(public_path)
+
+    items = listdir(current_directory)
+    for item in items:
+        file_path = path.join(current_directory, item)
+        if path.isfile(file_path):
+            print(f"Copying file {file_path}")
+            copy(file_path, public_path)
+        if path.isdir(file_path):
+           copy_static(file_path) 
+
+
 
 def main():
-    text = """
-# My main heading
-
-### A smaller heading
-
-* Item 
-* Another 
-* and this one is **special**
-
->This is a quote
->And Another
-
-```
-Some code
-```
-
-And this is
-just a paragraph
-
-
-"""
-    print(markdown_to_html_node(text).to_html())
-
-    
+    copy_static(".")
 main()
 
